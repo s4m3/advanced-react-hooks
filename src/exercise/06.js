32554a76157ab62e0db9883958f8846d1527d0e0
Simon Mary <sma@marketlogicsoftware.com>
2021-11-07 19:06:07 +0100

    exercise 6 - extra 1 complete

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,15 +3,18 @@
 
 import * as React from 'react'
 
+function formatDebugValues({ query, state }) {
+  return `\`${query}\` => ${state}`;
+}
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  // 🐨 call React.useDebugValue here.
-  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-  React.useDebugValue(`\`${query}\` => ${state}`);
+  React.useDebugValue({ query, state }, formatDebugValues);
 
   React.useEffect(() => {
     let mounted = true
     const mql = window.matchMedia(query)
+
     function onChange() {
       if (!mounted) {
         return
@@ -37,7 +40,7 @@ function Box() {
   const isSmall = useMedia('(max-width: 699px)')
   const color = isBig ? 'green' : isMedium ? 'yellow' : isSmall ? 'red' : null
 
-  return <div style={{width: 200, height: 200, backgroundColor: color}} />
+  return <div style={{ width: 200, height: 200, backgroundColor: color }} />
 }
 
 function App() {
