476e1f8e36630429a893ea1868725403d4c4060b
Simon Mary <sma@marketlogicsoftware.com>
2021-11-07 11:53:10 +0100

    extra 1 complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,23 +1,17 @@
-// useReducer: simple Counter
-// http://localhost:3000/isolated/exercise/01.js
+//1. 💯 accept the step as the action
 
 import * as React from 'react'
 
 const countReducer = (state, newState) => {
-  return newState;
+  return state + newState;
 }
 
 function Counter({ initialCount = 0, step = 1 }) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
   // const [count, setCount] = React.useState(initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount);
-
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
