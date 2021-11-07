b11db10b02bbfbb5005c6c52441e5884a53a198a
Simon Mary <sma@marketlogicsoftware.com>
2021-11-07 12:18:27 +0100

    extra 3 complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,8 +2,11 @@
 
 import * as React from 'react'
 
-const countReducer = (state, newState) => {
-  return newState;
+const countReducer = (state, action) => {
+  if (typeof action === 'function') {
+    return { ...state, ...action(state) }
+  }
+  return { ...state, ...action }
 }
 
 function Counter({ initialCount = 0, step = 1 }) {
@@ -11,8 +14,16 @@ function Counter({ initialCount = 0, step = 1 }) {
     count: initialCount,
   })
   const { count } = state
-  const increment = () => setState({ count: count + step })
-  return <button onClick={increment}>{count}</button>
+  const increment = () =>
+    setState(currentState => ({ count: currentState.count + step }))
+  const increment2 = () => setState({ count: count + step })
+  return (
+    <>
+      <button onClick={increment}>{count}</button>
+      <button onClick={increment2}>{count}</button>
+    </>
+
+  )
 }
 
 function App() {
